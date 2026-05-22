7bf5afcc74f9bcbc325284795296e9a3bb4da50e
vuonggiakhiem <khiemv75@gmail.com>
2026-05-22 20:44:03 +0700

    lol

--- kinhdich/sw.js
+++ kinhdich/sw.js
@@ -1,5 +1,30 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  }));
+const CACHE_NAME = "kinhdich-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+      })
+  );
 });
